eb1e2c047b4a85297ea8b36ea63a5f79981b1511
simone-danzeri <s.danzeri@icloud.com>
2024-05-16 13:10:56 +0200

    feat: created trait Weight

---
+++ Traits/Weight.php
@@ -0,0 +1,12 @@
+<?php
+trait Weight {
+    protected $weight;
+    
+    public function getWeight() {
+        $this->weight = $weight;
+    }
+    public function setWeight($newWeight) {
+        $this->weight = $newWeight;
+    }
+}
+?>
